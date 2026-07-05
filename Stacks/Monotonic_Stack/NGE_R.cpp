60cba7db79922e82914bac94658e7de4ae4edf6d
Gealardo <ayunverma902@gmail.com>
2026-07-05 23:19:25 +0530

    monotonic stack

---
+++ Stacks/Monotonic_Stack/NGE_R.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+vector<int> Next_Smaller_element_to_left(const vector<int>&arr){
+  int n=arr.size();
+  vector<int>ans(n,-1);
+  stack<int>st;
+  for(int i=n-1;i>=0;i--){
+    
+    while(!st.empty() && arr[i]>arr[st.top()]){
+      st.pop();
+    }
+    if(!st.empty()) ans[i]=arr[st.top()];
+    st.push(i);
+  }
+  return ans;
+
+}
+int main(){
+  vector<int>arr={1,6,2,4,9,0};
+  vector<int>ans=Next_Smaller_element_to_left(arr);
+  for( const auto x: ans){
+        cout<<x<<" ";
+  }
+  cout<<endl;
+}
