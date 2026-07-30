45c133e8b21087ac8cf9d40dbe4fbb00fa7ab304
Gealardo <ayunverma902@gmail.com>
2026-07-30 11:55:01 +0530

    construction of tree

---
+++ Trees2.O/01_Construct_Tree.cpp
@@ -0,0 +1,53 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+class Node{
+  public:
+   int data;
+   Node* left;
+   Node* right;
+   Node(int val){
+    data = val;
+    left = right = NULL;
+   }
+};
+
+int main(){
+
+ int rootval,leftval,rightval;
+ cout<<"Enter root value : ";
+ cin >> rootval;
+
+ // built tree
+ Node* root = new Node(rootval);
+ queue<Node*>q;
+ q.push(root);
+
+ while(!q.empty()){
+
+   Node* temp = q.front();
+   q.pop();
+   // for left
+   cout<<"Enter left value of "<<temp->data<<" : ";
+   cin >> leftval;
+
+   if(leftval!= -1){
+    temp->left = new Node(leftval);
+    q.push(temp->left);
+   }
+
+  // for right
+   cout<<"Enter Right value of "<<temp->data<<" : ";
+   cin >> rightval;
+
+   if(rightval!= -1){
+    temp->right = new Node(rightval);
+    q.push(temp->right);
+
+   }
+
+ }
+ cout<<" Tree constructed ";
+
+}
