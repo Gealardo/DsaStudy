7e449841c84f24318fec4eb7babef160f5c840e8
Gealardo <ayunverma902@gmail.com>
2026-08-18 18:40:50 +0530

    ||

---
+++ 00_LeetCode/Array_Ques/219.contains-duplicate-ii.cpp
@@ -0,0 +1,24 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+/*
+ * @lc app=leetcode id=219 lang=cpp
+ *
+ * [219] Contains Duplicate II
+ */
+
+// @lc code=start
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+       int n=nums.size();
+        unordered_map<int,int>mp;
+        for(int i=0;i<n;i++){
+            if((mp.find(nums[i])!=mp.end()) && abs(i-mp[nums[i]])<=k) return true;
+            mp[nums[i]]=i;
+        }
+        return false;
+    }
+};
+// @lc code=end
+
