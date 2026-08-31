5555049f0d8065c85bc811d80dd57e37870ec024
Gealardo <ayunverma902@gmail.com>
2026-08-31 16:16:45 +0530

    Added Solution - GfG to GitHub

---
+++ Difficulty: Medium/Minimum Insertions for Palindrome/minimum-insertions-for-palindrome.cpp
@@ -0,0 +1,25 @@
+class Solution {
+  public:
+    int LCS(string &s1,string &s2){
+        int n=s1.length(); int m=s2.length();
+        if(n==0 || m==0) return 0;
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++){
+            for(int j=1;j<=m;j++){
+                if(s1[i-1]==s2[j-1]) dp[i][j]= 1+dp[i-1][j-1];
+                else dp[i][j] = 0+max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+    }
+    int LPS(string &s){
+        string s1=s;
+        string s2=s;
+        reverse(s2.begin(),s2.end());
+        return LCS(s1,s2);
+    }
+    int findMinInsertions(string &s) {
+        int n=s.length();
+        return n - LPS(s) ;
+    }
+};
